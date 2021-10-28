5044ff230dd239dc4a5d8925f068d5882e7292ff
Winfried <ozgurarslln@icloud.com>
2021-10-28 11:39:03 +0300

    Added New Script

---
+++ windows11/virtualmachine.ps1
@@ -0,0 +1,6 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+echo "I activate the Virtual Machine Platform."
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+Pause
